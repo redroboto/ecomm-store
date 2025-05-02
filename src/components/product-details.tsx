b399984227fa86fafe86d795d2a5ca28d77c5b1b
redroboto <vince.pleyto@gmail.com>
2025-05-02 21:01:22 +0800

    added zustand for state management in the store folder. Added a useStore object that holds the items, functions such as addItem, deductItem, and clearItems

--- src/components/product-details.tsx
+++ src/components/product-details.tsx
@@ -1,12 +1,28 @@
+"use client"
+
 import Stripe from "stripe"
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { useStore } from "@/store/cart-store";
 
 interface Props{
     product: Stripe.Product;
 }
 const ProductDetails = ({product} : Props) => {
     const price = product.default_price as Stripe.Price;
+    const {items, addItem, deductItem} = useStore();
+    const cartItem = items.find((i) => i.id === product.id);
+    const quantity = cartItem?.quantity;
+
+    const handleAddItem = () => {
+        addItem({
+            id: product.id,
+            name: product.name,
+            price: price.unit_amount as number,
+            imageUrl: product.images? product.images[0] : null,
+            quantity: 1,
+        });
+    };
 
     return (
         <div className="grid grid-cols-2 space-x-2">
@@ -21,7 +37,9 @@ const ProductDetails = ({product} : Props) => {
                 <p>{product.description}</p>
                 <span className="font-bold">{price && price.unit_amount && `$ ${(price.unit_amount / 100).toFixed(2)}`}</span>
                 <div className="flex space-x-3 items-center">
-                    <Button>+</Button><span>0</span><Button>-</Button>
+                    <Button onClick={() => deductItem(product.id)}>-</Button>
+                    <span>{quantity ? quantity : 0}</span>
+                    <Button onClick={handleAddItem}>+</Button>
                 </div>
             </div>
         </div>
---
+++ src/store/cart-store.ts
@@ -0,0 +1,49 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface CartItem {
+    id: string,
+    name: string,
+    price: number,
+    imageUrl: string | null,
+    quantity: number,
+}
+
+interface CartStore {
+    items: CartItem[],
+    addItem: (item: CartItem) => void,
+    deductItem: (id: string) => void,
+    clearItems: () => void,
+
+}
+
+export const useStore = create<CartStore>()(persist(
+    (set) => ({
+        items: [],
+        addItem: (item) => set((state) => {
+            const existing = state.items.find((i) => i.id === item.id);
+
+            if (existing){
+                return {
+                    items: state.items.map((i) => {
+                        return i.id === item.id ? {...i, quantity: i.quantity + item.quantity} : i
+                    })
+                }
+            }
+
+            return {items: [...state.items, item]};
+
+
+        }),
+        deductItem: (id) => set((state) => {
+            return {
+                items: state.items.map((item) => {
+                    return item.id === id ? {...item, quantity: item.quantity - 1} : item
+                }).filter((item) => item.quantity > 0)
+            }
+        }),
+        clearItems: () => set((state) => {
+            return {items: []}
+        }),
+    }), { name: "cart"}
+) )
